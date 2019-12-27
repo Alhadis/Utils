bd2893ec874aeee60e1377d477934db939b4dd2b
Alhadis <gardnerjohng@gmail.com>
2019-12-27 18:00:36 +1100

    Add `sha1()` tests and ensure 40-character hashes
    The fixture `rgba/00009a89.png` produced a checksum with a leading zero,
    which wasn't being prepended by `String.prototype.toString`.

--- test/binary.mjs
+++ test/binary.mjs
@@ -192,6 +192,67 @@ describe("Byte-level functions", () => {
 		});
 	});
 	
+	describe("sha1()", () => {
+		const {sha1} = utils;
+		const $ = s => s.split("").map(x => x.charCodeAt(0));
+		it("computes checksums of 3-byte messages", () => {
+			expect(sha1([120, 121, 122])).to.equal("66b27417d37e024c46526c2f6d358a754fc552f3");
+			expect(sha1([0x1, 0x2, 0x3])).to.equal("7037807198c22a7d2b0807371d763779a84fdfcf");
+			expect(sha1([255, 255, 255])).to.equal("78670e88a9c2c711124471d2f24a8dbc8ce5dba9");
+			expect(sha1([128, 200, 0x7])).to.equal("2d1664fac047f29bd5e4f66435616e0e43e12a16");
+			expect(sha1([0x0, 0x0, 0x0])).to.equal("29e2dcfbb16f63bb0254df7585a15bb6fb5e927d");
+		});
+		it("computes checksums of empty messages", () => {
+			expect(sha1([])).to.equal("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+		});
+		it("computes checksums of pangrams", () => {
+			expect(sha1($("Glib jocks quiz nymph to vex dwarf.")))        .to.equal("5c7003e6d645a88d7e0717d3ea85f3e26350d8c7");
+			expect(sha1($("Sphinx of black quartz, judge my vow.")))      .to.equal("77df869f7e054756bcce6d85dbb35f01cf3ea24a");
+			expect(sha1($("How vexingly quick daft zebras jump!")))       .to.equal("7ab03e8daaf316ebedf1b8cfb63749a3202bbf47");
+			expect(sha1($("The five boxing wizards jump quickly.")))      .to.equal("3d91b05a0c8bc749bdcaf63f0179b6017eba0383");
+			expect(sha1($("Pack my box with five dozen liquor jugs.")))   .to.equal("a7e9cf930cae010f36bc252d59bf391ecc8b52d6");
+			expect(sha1($("The quick brown fox jumps over the lazy dog"))).to.equal("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+			expect(sha1($("The quick brown fox jumps over the lazy cog"))).to.equal("de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3");
+			expect(sha1($("Jived fox nymph grabs quick waltz.")))         .to.equal("368fd45c3a192001c09cfd84ff4afa718c86b747");
+		});
+		it("computes checksums of binary blocks", () => {
+			const zeroes = new Uint8Array(1024);
+			const all128 = new Uint8Array(2048).fill(0x80);
+			const all255 = new Uint8Array(3071).fill(0xFF);
+			const latin1 = new Uint8Array(4096).map((n, index) => index % 255);
+			expect(sha1(zeroes)).to.equal("60cacbf3d72e1e7834203da608037b1bf83b40e8");
+			expect(sha1(all128)).to.equal("68e1ff2951f5d5f074613fa5480680f11dd1495d");
+			expect(sha1(all255)).to.equal("e32a0982173eef5e9318f562b35fd1f43c85fb7c");
+			expect(sha1(latin1)).to.equal("71bf658ae6ab40a70defe0d63f95f686870b754f");
+		});
+		it("computes checksums of PNG fixtures", () => {
+			expect(sha1($(file("rgba/00000000.png")))).to.equal("f8d88e6e02b634ea9cc384d33596c4f590b6ae31");
+			expect(sha1($(file("rgba/00000040.png")))).to.equal("fd69eead19d508fa659c6edfe3a0f8681ecf28d0");
+			expect(sha1($(file("rgba/000000ff.png")))).to.equal("ec6f28cd5ebed9bc95053fed3a2018bfff3e0596");
+			expect(sha1($(file("rgba/00009a89.png")))).to.equal("0fde443c129e30c91d2d6824397bd2fbd06c51a1");
+			expect(sha1($(file("rgba/0000ff00.png")))).to.equal("0762f078c0f3014dabe87bad3fe2f35d3dea64b3");
+			expect(sha1($(file("rgba/0000ffff.png")))).to.equal("9ee13be59305affc103d687909496822ac40d920");
+			expect(sha1($(file("rgba/0085003a.png")))).to.equal("baa685e7cd63395c21daa8c0e18a4ed7975de8d4");
+			expect(sha1($(file("rgba/00ff0000.png")))).to.equal("e7702eb43ead2ed16aa9a90b890132b6c76a9db4");
+			expect(sha1($(file("rgba/00ff00ff.png")))).to.equal("4b594c3fa919b925992d97255ca29bf1e25c08a6");
+			expect(sha1($(file("rgba/3a3b3c00.png")))).to.equal("d86184312b399508e6add30a707ba4ff4e03eb3a");
+			expect(sha1($(file("rgba/3a3b3c6f.png")))).to.equal("ecbab7ef0ee4d6402e6bfcdcd10614a3386d0bf6");
+			expect(sha1($(file("rgba/7f7f7fff.png")))).to.equal("4f7e15871a7156ee6a0791960ece126bc2f836e6");
+			expect(sha1($(file("rgba/af000064.png")))).to.equal("98ea5e764168c160582f0ed9a921ab54b5c5ef84");
+			expect(sha1($(file("rgba/bbaaff00.png")))).to.equal("a66ff64d8b139ea7c303850bad114f0bf7b6cb4b");
+			expect(sha1($(file("rgba/bbaaff0a.png")))).to.equal("475df629ebbed4296b5b2d20b0eef8f6b603d260");
+			expect(sha1($(file("rgba/bbaaffff.png")))).to.equal("094e8d1ff70ae139699a409c086f07964cb2417a");
+			expect(sha1($(file("rgba/bbffdd00.png")))).to.equal("b5f2ff741439aa66a2b1c6029c94688a5fe220d0");
+			expect(sha1($(file("rgba/bbffdd30.png")))).to.equal("246042095ef802ef3f59b6e46fcf8dbd1a29794e");
+			expect(sha1($(file("rgba/bbffddff.png")))).to.equal("65832700005eea524e4eacfd2d1244f50c5c1fbf");
+			expect(sha1($(file("rgba/ff000000.png")))).to.equal("75d60144d0907b5d300ec580904ad052e2812d43");
+			expect(sha1($(file("rgba/ff0000ff.png")))).to.equal("b31f0478d976ce5749f86344211aeeba41de065e");
+			expect(sha1($(file("rgba/ffffff00.png")))).to.equal("950408bcffbed5942be5534b0c6437a46524d617");
+			expect(sha1($(file("rgba/ffffff7f.png")))).to.equal("4a068096b7bdc92ac98d1d799e5ebc503691f119");
+			expect(sha1($(file("rgba/ffffffff.png")))).to.equal("5372946ce021a6956209135e2426bceb3a47e239");
+		});
+	});
+	
 	describe("uint16ToBytes()", () => {
 		const {uint16ToBytes} = utils;
 		it("decodes integers in big-endian order", () => {
