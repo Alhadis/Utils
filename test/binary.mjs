9dcdd32ee2681522652c0d508d597643745cf5f8
Alhadis <gardnerjohng@gmail.com>
2020-01-12 18:45:22 +1100

    Refactor binary functions to expect bytes as input

--- test/binary.mjs
+++ test/binary.mjs
@@ -6,14 +6,10 @@ import * as utils      from "../index.mjs";
 
 describe("Byte-level functions", () => {
 	const dir  = dirname(fileURLToPath(import.meta.url));
-	const file = path => readFileSync(join(dir, "fixtures", ...path.split("/")), {encoding: "binary"});
+	const file = path => [...readFileSync(join(dir, "fixtures", ...path.split("/")), {encoding: null})];
 	
 	describe("adler32()", () => {
 		const {adler32} = utils;
-		it("computes checksums of string values", () => {
-			expect(adler32("foo-bar")).to.equal(0xAA402A7);
-			expect(adler32("Wikipedia")).to.equal(0x11E60398);
-		});
 		it("computes checksums of byte arrays", () => {
 			const A = [0x66, 0x6F, 0x6F, 0x2D, 0x62, 0x61, 0x72];
 			const B = [0x57, 0x69, 0x6B, 0x69, 0x70, 0x65, 0x64, 0x69, 0x61];
@@ -35,35 +31,23 @@ describe("Byte-level functions", () => {
 		});
 	});
 	
-	// Byte-arrays shared by base64-related functions
-	const bytesFoo  = [0x46, 0x6F, 0x6F, 0x42, 0x61, 0x72];
-	const bytesCafe = [0x63, 0xE1, 0x66, 0xE9, 0x62, 0xE1, 0x62, 0xE9];
-	const bytesRaw  = [0x89, 0x01, 0x00, 0x8F, 0xFF, 0xFE, 0x00];
-	const stringRaw = "\x89\x01\0\x8F\xFF\xFE\0";
-	
 	describe("base64Decode()", () => {
 		const {base64Decode} = utils;
-		it("decodes 7-bit ASCII strings",    () => expect(base64Decode("Rm9vQmFy")).to.equal("FooBar"));
-		it("decodes 7-bit ASCII bytes",      () => expect(base64Decode("Rm9vQmFy", true)).to.eql(bytesFoo));
-		it("decodes extended ASCII strings", () => expect(base64Decode("Y+Fm6WLhYuk=")).to.equal("cáfébábé"));
-		it("decodes extended ASCII bytes",   () => expect(base64Decode("Y+Fm6WLhYuk=", true)).to.eql(bytesCafe));
-		it("decodes raw binary strings",     () => expect(base64Decode("iQEAj//+AA==")).to.equal(stringRaw));
-		it("decodes raw binary bytes",       () => expect(base64Decode("iQEAj//+AA==", true)).to.eql(bytesRaw));
-		it("decodes UTF-8 as Latin-1",       () => expect(base64Decode("Y8OhZsOpYsOhYsOp")).to.equal("cÃ¡fÃ©bÃ¡bÃ©"));
+		it("decodes ASCII",   () => expect(base64Decode("Rm9vQmFy")).to.eql([0x46, 0x6F, 0x6F, 0x42, 0x61, 0x72]));
+		it("decodes Latin-1", () => expect(base64Decode("Y+Fm6WLhYuk=")).to.eql([0x63, 0xE1, 0x66, 0xE9, 0x62, 0xE1, 0x62, 0xE9]));
+		it("decodes binary",  () => expect(base64Decode("iQEAj//+AA==")).to.eql([0x89, 0x01, 0x00, 0x8F, 0xFF, 0xFE, 0x00]));
+		it("decodes UTF-8",   () => expect(base64Decode("8J+Ygg==")).to.eql([0xF0, 0x9F, 0x98, 0x82]));
 	});
 	
 	describe("base64Encode()", () => {
 		const {base64Encode} = utils;
-		it("encodes 7-bit ASCII strings",    () => expect(base64Encode("FooBar")).to.equal("Rm9vQmFy"));
-		it("encodes 7-bit ASCII bytes",      () => expect(base64Encode(bytesFoo)).to.equal("Rm9vQmFy"));
-		it("encodes extended ASCII strings", () => expect(base64Encode("cáfébábé")).to.equal("Y+Fm6WLhYuk="));
-		it("encodes extended ASCII bytes",   () => expect(base64Encode(bytesCafe)).to.equal("Y+Fm6WLhYuk="));
-		it("encodes raw binary strings",     () => expect(base64Encode(stringRaw)).to.equal("iQEAj//+AA=="));
-		it("encodes raw binary bytes",       () => expect(base64Encode(bytesRaw)).to.equal("iQEAj//+AA=="));
-		it("encodes PNG images correctly",   () => {
-			const json = JSON.parse(file("base64/rgba.json"));
-			for(const key in json)
-				expect(base64Encode(file(key))).to.equal(json[key]);
+		it("encodes ASCII",   () => expect(base64Encode([0x46, 0x6F, 0x6F, 0x42, 0x61, 0x72])).to.equal("Rm9vQmFy"));
+		it("encodes Latin-1", () => expect(base64Encode([0x63, 0xE1, 0x66, 0xE9, 0x62, 0xE1, 0x62, 0xE9])).to.equal("Y+Fm6WLhYuk="));
+		it("encodes binary",  () => expect(base64Encode([0x89, 0x01, 0x00, 0x8F, 0xFF, 0xFE, 0x00])).to.equal("iQEAj//+AA=="));
+		it("encodes PNGs",    () => {
+			const json = JSON.parse(String.fromCharCode(...file("base64/rgba.json")));
+			for(const [png, encoded] of Object.entries(json))
+				expect(base64Encode(file(png))).to.equal(encoded);
 		});
 	});
 	
@@ -136,10 +120,6 @@ describe("Byte-level functions", () => {
 	
 	describe("crc32()", () => {
 		const {crc32} = utils;
-		it("computes CRCs of string values", () => {
-			expect(crc32("Foo123")).to.equal(0x67EDF5DB);
-			expect(crc32("\0\x1B\xA4\0\x07")).to.equal(0x12F479C);
-		});
 		it("computes CRCs of byte arrays", () => {
 			expect(crc32([0x46, 0x6F, 0x6F, 0x31, 0x32, 0x33])).to.equal(0x67EDF5DB);
 			expect(crc32([0x00, 0x1B, 0xA4, 0x00, 0x07])).to.equal(0x12F479C);
@@ -162,34 +142,34 @@ describe("Byte-level functions", () => {
 	describe("rgba()", () => {
 		const {rgba} = utils;
 		describe("Solid colours", () => {
-			it("generates red pixels",     () => expect(rgba(0xFF, 0x00, 0x00, 0xFF)).to.equal(file("rgba/ff0000ff.png")));
-			it("generates green pixels",   () => expect(rgba(0x00, 0xFF, 0x00, 0xFF)).to.equal(file("rgba/00ff00ff.png")));
-			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0xFF, 0xFF)).to.equal(file("rgba/0000ffff.png")));
-			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0xFF)).to.equal(file("rgba/ffffffff.png")));
-			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0xFF)).to.equal(file("rgba/000000ff.png")));
-			it("generates grey pixels",    () => expect(rgba(0x7F, 0x7F, 0x7F, 0xFF)).to.equal(file("rgba/7f7f7fff.png")));
-			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0xFF)).to.equal(file("rgba/bbffddff.png")));
-			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0xFF)).to.equal(file("rgba/bbaaffff.png")));
+			it("generates red pixels",     () => expect(rgba(0xFF, 0x00, 0x00, 0xFF)).to.eql(file("rgba/ff0000ff.png")));
+			it("generates green pixels",   () => expect(rgba(0x00, 0xFF, 0x00, 0xFF)).to.eql(file("rgba/00ff00ff.png")));
+			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0xFF, 0xFF)).to.eql(file("rgba/0000ffff.png")));
+			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0xFF)).to.eql(file("rgba/ffffffff.png")));
+			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0xFF)).to.eql(file("rgba/000000ff.png")));
+			it("generates grey pixels",    () => expect(rgba(0x7F, 0x7F, 0x7F, 0xFF)).to.eql(file("rgba/7f7f7fff.png")));
+			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0xFF)).to.eql(file("rgba/bbffddff.png")));
+			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0xFF)).to.eql(file("rgba/bbaaffff.png")));
 		});
 		describe("Translucent colours", () => {
-			it("generates red pixels",     () => expect(rgba(0xAF, 0x00, 0x00, 0x64)).to.equal(file("rgba/af000064.png")));
-			it("generates green pixels",   () => expect(rgba(0x00, 0x85, 0x00, 0x3A)).to.equal(file("rgba/0085003a.png")));
-			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0x9A, 0x89)).to.equal(file("rgba/00009a89.png")));
-			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0x7F)).to.equal(file("rgba/ffffff7f.png")));
-			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0x40)).to.equal(file("rgba/00000040.png")));
-			it("generates grey pixels",    () => expect(rgba(0x3A, 0x3B, 0x3C, 0x6F)).to.equal(file("rgba/3a3b3c6f.png")));
-			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0x30)).to.equal(file("rgba/bbffdd30.png")));
-			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0x0A)).to.equal(file("rgba/bbaaff0a.png")));
+			it("generates red pixels",     () => expect(rgba(0xAF, 0x00, 0x00, 0x64)).to.eql(file("rgba/af000064.png")));
+			it("generates green pixels",   () => expect(rgba(0x00, 0x85, 0x00, 0x3A)).to.eql(file("rgba/0085003a.png")));
+			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0x9A, 0x89)).to.eql(file("rgba/00009a89.png")));
+			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0x7F)).to.eql(file("rgba/ffffff7f.png")));
+			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0x40)).to.eql(file("rgba/00000040.png")));
+			it("generates grey pixels",    () => expect(rgba(0x3A, 0x3B, 0x3C, 0x6F)).to.eql(file("rgba/3a3b3c6f.png")));
+			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0x30)).to.eql(file("rgba/bbffdd30.png")));
+			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0x0A)).to.eql(file("rgba/bbaaff0a.png")));
 		});
 		describe("100% transparency", () => {
-			it("generates red pixels",     () => expect(rgba(0xFF, 0x00, 0x00, 0x00)).to.equal(file("rgba/ff000000.png")));
-			it("generates green pixels",   () => expect(rgba(0x00, 0xFF, 0x00, 0x00)).to.equal(file("rgba/00ff0000.png")));
-			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0xFF, 0x00)).to.equal(file("rgba/0000ff00.png")));
-			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0x00)).to.equal(file("rgba/ffffff00.png")));
-			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0x00)).to.equal(file("rgba/00000000.png")));
-			it("generates grey pixels",    () => expect(rgba(0x3A, 0x3B, 0x3C, 0x00)).to.equal(file("rgba/3a3b3c00.png")));
-			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0x00)).to.equal(file("rgba/bbffdd00.png")));
-			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0x00)).to.equal(file("rgba/bbaaff00.png")));
+			it("generates red pixels",     () => expect(rgba(0xFF, 0x00, 0x00, 0x00)).to.eql(file("rgba/ff000000.png")));
+			it("generates green pixels",   () => expect(rgba(0x00, 0xFF, 0x00, 0x00)).to.eql(file("rgba/00ff0000.png")));
+			it("generates blue pixels",    () => expect(rgba(0x00, 0x00, 0xFF, 0x00)).to.eql(file("rgba/0000ff00.png")));
+			it("generates white pixels",   () => expect(rgba(0xFF, 0xFF, 0xFF, 0x00)).to.eql(file("rgba/ffffff00.png")));
+			it("generates black pixels",   () => expect(rgba(0x00, 0x00, 0x00, 0x00)).to.eql(file("rgba/00000000.png")));
+			it("generates grey pixels",    () => expect(rgba(0x3A, 0x3B, 0x3C, 0x00)).to.eql(file("rgba/3a3b3c00.png")));
+			it("generates #BBFFDD pixels", () => expect(rgba(0xBB, 0xFF, 0xDD, 0x00)).to.eql(file("rgba/bbffdd00.png")));
+			it("generates #BBAAFF pixels", () => expect(rgba(0xBB, 0xAA, 0xFF, 0x00)).to.eql(file("rgba/bbaaff00.png")));
 		});
 	});
 	
@@ -219,7 +199,6 @@ describe("Byte-level functions", () => {
 	
 	describe("sha1()", () => {
 		const {sha1} = utils;
-		const $ = s => s.split("").map(x => x.charCodeAt(0));
 		it("computes checksums of 3-byte messages", () => {
 			expect(sha1([120, 121, 122])).to.equal("66b27417d37e024c46526c2f6d358a754fc552f3");
 			expect(sha1([0x1, 0x2, 0x3])).to.equal("7037807198c22a7d2b0807371d763779a84fdfcf");
@@ -231,6 +210,7 @@ describe("Byte-level functions", () => {
 			expect(sha1([])).to.equal("da39a3ee5e6b4b0d3255bfef95601890afd80709");
 		});
 		it("computes checksums of pangrams", () => {
+			const $ = s => s.split("").map(x => x.charCodeAt(0));
 			expect(sha1($("Glib jocks quiz nymph to vex dwarf.")))        .to.equal("5c7003e6d645a88d7e0717d3ea85f3e26350d8c7");
 			expect(sha1($("Sphinx of black quartz, judge my vow.")))      .to.equal("77df869f7e054756bcce6d85dbb35f01cf3ea24a");
 			expect(sha1($("How vexingly quick daft zebras jump!")))       .to.equal("7ab03e8daaf316ebedf1b8cfb63749a3202bbf47");
@@ -251,30 +231,30 @@ describe("Byte-level functions", () => {
 			expect(sha1(latin1)).to.equal("71bf658ae6ab40a70defe0d63f95f686870b754f");
 		});
 		it("computes checksums of PNG fixtures", () => {
-			expect(sha1($(file("rgba/00000000.png")))).to.equal("f8d88e6e02b634ea9cc384d33596c4f590b6ae31");
-			expect(sha1($(file("rgba/00000040.png")))).to.equal("fd69eead19d508fa659c6edfe3a0f8681ecf28d0");
-			expect(sha1($(file("rgba/000000ff.png")))).to.equal("ec6f28cd5ebed9bc95053fed3a2018bfff3e0596");
-			expect(sha1($(file("rgba/00009a89.png")))).to.equal("0fde443c129e30c91d2d6824397bd2fbd06c51a1");
-			expect(sha1($(file("rgba/0000ff00.png")))).to.equal("0762f078c0f3014dabe87bad3fe2f35d3dea64b3");
-			expect(sha1($(file("rgba/0000ffff.png")))).to.equal("9ee13be59305affc103d687909496822ac40d920");
-			expect(sha1($(file("rgba/0085003a.png")))).to.equal("baa685e7cd63395c21daa8c0e18a4ed7975de8d4");
-			expect(sha1($(file("rgba/00ff0000.png")))).to.equal("e7702eb43ead2ed16aa9a90b890132b6c76a9db4");
-			expect(sha1($(file("rgba/00ff00ff.png")))).to.equal("4b594c3fa919b925992d97255ca29bf1e25c08a6");
-			expect(sha1($(file("rgba/3a3b3c00.png")))).to.equal("d86184312b399508e6add30a707ba4ff4e03eb3a");
-			expect(sha1($(file("rgba/3a3b3c6f.png")))).to.equal("ecbab7ef0ee4d6402e6bfcdcd10614a3386d0bf6");
-			expect(sha1($(file("rgba/7f7f7fff.png")))).to.equal("4f7e15871a7156ee6a0791960ece126bc2f836e6");
-			expect(sha1($(file("rgba/af000064.png")))).to.equal("98ea5e764168c160582f0ed9a921ab54b5c5ef84");
-			expect(sha1($(file("rgba/bbaaff00.png")))).to.equal("a66ff64d8b139ea7c303850bad114f0bf7b6cb4b");
-			expect(sha1($(file("rgba/bbaaff0a.png")))).to.equal("475df629ebbed4296b5b2d20b0eef8f6b603d260");
-			expect(sha1($(file("rgba/bbaaffff.png")))).to.equal("094e8d1ff70ae139699a409c086f07964cb2417a");
-			expect(sha1($(file("rgba/bbffdd00.png")))).to.equal("b5f2ff741439aa66a2b1c6029c94688a5fe220d0");
-			expect(sha1($(file("rgba/bbffdd30.png")))).to.equal("246042095ef802ef3f59b6e46fcf8dbd1a29794e");
-			expect(sha1($(file("rgba/bbffddff.png")))).to.equal("65832700005eea524e4eacfd2d1244f50c5c1fbf");
-			expect(sha1($(file("rgba/ff000000.png")))).to.equal("75d60144d0907b5d300ec580904ad052e2812d43");
-			expect(sha1($(file("rgba/ff0000ff.png")))).to.equal("b31f0478d976ce5749f86344211aeeba41de065e");
-			expect(sha1($(file("rgba/ffffff00.png")))).to.equal("950408bcffbed5942be5534b0c6437a46524d617");
-			expect(sha1($(file("rgba/ffffff7f.png")))).to.equal("4a068096b7bdc92ac98d1d799e5ebc503691f119");
-			expect(sha1($(file("rgba/ffffffff.png")))).to.equal("5372946ce021a6956209135e2426bceb3a47e239");
+			expect(sha1(file("rgba/00000000.png"))).to.equal("f8d88e6e02b634ea9cc384d33596c4f590b6ae31");
+			expect(sha1(file("rgba/00000040.png"))).to.equal("fd69eead19d508fa659c6edfe3a0f8681ecf28d0");
+			expect(sha1(file("rgba/000000ff.png"))).to.equal("ec6f28cd5ebed9bc95053fed3a2018bfff3e0596");
+			expect(sha1(file("rgba/00009a89.png"))).to.equal("0fde443c129e30c91d2d6824397bd2fbd06c51a1");
+			expect(sha1(file("rgba/0000ff00.png"))).to.equal("0762f078c0f3014dabe87bad3fe2f35d3dea64b3");
+			expect(sha1(file("rgba/0000ffff.png"))).to.equal("9ee13be59305affc103d687909496822ac40d920");
+			expect(sha1(file("rgba/0085003a.png"))).to.equal("baa685e7cd63395c21daa8c0e18a4ed7975de8d4");
+			expect(sha1(file("rgba/00ff0000.png"))).to.equal("e7702eb43ead2ed16aa9a90b890132b6c76a9db4");
+			expect(sha1(file("rgba/00ff00ff.png"))).to.equal("4b594c3fa919b925992d97255ca29bf1e25c08a6");
+			expect(sha1(file("rgba/3a3b3c00.png"))).to.equal("d86184312b399508e6add30a707ba4ff4e03eb3a");
+			expect(sha1(file("rgba/3a3b3c6f.png"))).to.equal("ecbab7ef0ee4d6402e6bfcdcd10614a3386d0bf6");
+			expect(sha1(file("rgba/7f7f7fff.png"))).to.equal("4f7e15871a7156ee6a0791960ece126bc2f836e6");
+			expect(sha1(file("rgba/af000064.png"))).to.equal("98ea5e764168c160582f0ed9a921ab54b5c5ef84");
+			expect(sha1(file("rgba/bbaaff00.png"))).to.equal("a66ff64d8b139ea7c303850bad114f0bf7b6cb4b");
+			expect(sha1(file("rgba/bbaaff0a.png"))).to.equal("475df629ebbed4296b5b2d20b0eef8f6b603d260");
+			expect(sha1(file("rgba/bbaaffff.png"))).to.equal("094e8d1ff70ae139699a409c086f07964cb2417a");
+			expect(sha1(file("rgba/bbffdd00.png"))).to.equal("b5f2ff741439aa66a2b1c6029c94688a5fe220d0");
+			expect(sha1(file("rgba/bbffdd30.png"))).to.equal("246042095ef802ef3f59b6e46fcf8dbd1a29794e");
+			expect(sha1(file("rgba/bbffddff.png"))).to.equal("65832700005eea524e4eacfd2d1244f50c5c1fbf");
+			expect(sha1(file("rgba/ff000000.png"))).to.equal("75d60144d0907b5d300ec580904ad052e2812d43");
+			expect(sha1(file("rgba/ff0000ff.png"))).to.equal("b31f0478d976ce5749f86344211aeeba41de065e");
+			expect(sha1(file("rgba/ffffff00.png"))).to.equal("950408bcffbed5942be5534b0c6437a46524d617");
+			expect(sha1(file("rgba/ffffff7f.png"))).to.equal("4a068096b7bdc92ac98d1d799e5ebc503691f119");
+			expect(sha1(file("rgba/ffffffff.png"))).to.equal("5372946ce021a6956209135e2426bceb3a47e239");
 		});
 	});
 	
